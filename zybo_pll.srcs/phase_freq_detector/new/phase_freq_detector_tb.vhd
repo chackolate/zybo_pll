ca94870ee7b747c14a6cb500916e78271eb3aab0
chackolate0 <atchacko@udel.edu>
2022-02-16 13:35:13 -0500

    wcfg

--- zybo_pll.srcs/phase_freq_detector/new/phase_freq_detector_tb.vhd
+++ zybo_pll.srcs/phase_freq_detector/new/phase_freq_detector_tb.vhd
@@ -24,11 +24,6 @@ use IEEE.STD_LOGIC_1164.all;
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity phase_freq_detector_tb is
 	--  Port ( );
 end phase_freq_detector_tb;
